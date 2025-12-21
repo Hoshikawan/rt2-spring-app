9cf2d05b2fa36e674eea4d065c2a88c8ab245f24
hoshikawa <hoshikawa.shion@techbase.jp>
2025-12-21 19:28:46 +0900

    個人開発演習_発展機能：独自機能実装（JS）新規社員登録ホバー時に色付け_修正

--- spring_crud_exc5_name/src/main/resources/static/js/motion_layout.js
+++ spring_crud_exc5_name/src/main/resources/static/js/motion_layout.js
@@ -1,32 +1,22 @@
 // HTMLにあるid="regist_link"の要素を取得する
 const regist_link = document.getElementById('regist_link');
 
-// 要素が存在するか確認 ページによっては存在しない場合があるため、エラー防止のためのif文
+// 要素が存在するか確認  Javaでいうif(regist_link != null)とほぼ同じ意味
+// HTMLからregist_linkが消えたときに、nullに対してメソッドを呼ぼうとしているからエラーになってしまう
+// エラーが出ないようにするために、「もしregist_linkがあったら」とif文を記述している
 if (regist_link){
-	// マウスがリンクの上に乗った時の処理  mouseenter = ホバーした瞬間
+	// マウスがリンクの上に乗った時の処理  mouseenter = ホバーした瞬間（カーソルが要素の上に乗った瞬間）
 	regist_link.addEventListener("mouseenter", () => {
-		// aタグに「hover」というクラスを追加する→CSSのa.hoverが有効になり、色が変わる
-		regist_link.classList.add("hover");
+//		 ↓CSSで色を決めていたらこの記述が必要だったが、今回はJSで色指定の記述をしたので必要なくなった
+//		regist_link.classList.add("hover");
 		regist_link.style.color = "#0bd";
 	});
 
-	// マウスがリンクから離れたときの処理  mouseleave = ホバー解除
+	// マウスがリンクから離れたときの処理  mouseleave = ホバー解除（カーソルが要素から離れた瞬間）
 	regist_link.addEventListener("mouseleave", () => {
 		// 追加したhoverクラスを削除する→元の文字色に戻る
-		regist_link.classList.remove("hover");
+//		regist_link.classList.remove("hover");
 		regist_link.style.color = "";
 	});
 }
 
-//const regist_link = document.getElementById('regist_link');
-//const regist = regist_link.innerHTML;
-//
-////if (regist_link){
-//	regist.addEventListener("mouseenter", () => {
-//		regist.classList.add("hover");
-//	});
-//
-//	regist.addEventListener("mouseleave", () => {
-//		regist.classList.remove("hover");
-//	});
-////}
